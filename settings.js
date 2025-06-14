3c02bbe3cf38894cdcedcf6692490205b5027999
Sayfull XD <129086018+S4yfullXD@users.noreply.github.com>
2025-06-14 13:24:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'SayfullXD'// yourname
+global.MONGO_DB_URI = "mongodb+srv://mongodb-bot:PZt5K_8qLpamq2,@cluster0.rkmfcrf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
 global.your_email = "" //email
 global.email_password = "" //application password email
